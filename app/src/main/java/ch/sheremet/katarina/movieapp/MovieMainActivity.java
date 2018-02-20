c61a599405ea4a415034fc5fbf0cda7db5f09476
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-02-20 14:17:11 +0100

    Initial commit

---
+++ app/src/main/java/ch/sheremet/katarina/movieapp/MovieMainActivity.java
@@ -0,0 +1,13 @@
+package ch.sheremet.katarina.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_main);
+    }
+}
